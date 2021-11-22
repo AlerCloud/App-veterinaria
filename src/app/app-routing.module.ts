d6cb3945170f022ad5779144af82628830fd161a
AlerCloud <al.uriber@duocuc.cl>
2021-11-22 15:27:00 -0300

    Se creo un login para dar inicio al proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path:'registro',
+    component: RegistroComponent
+
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+ 
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
